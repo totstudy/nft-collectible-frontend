067ecfc541f67425d7bd9e9dca97cc20db8856ad
totstudy <5656a7green@gmail.com>
2022-12-27 05:29:06 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,176 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState, Fragment } from "react";
+import squirrelImg from './assets/rinkeby_squirrels.gif';
+import { ethers } from 'ethers';
+import contract from './contracts/NFTCollectible.json';
+// import { Fragment } from 'react/cjs/react.production.min';
+// import { Fragment } from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// Constants
+const OPENSEA_LINK = 'https://testnets.opensea.io/collection/';
+const MEDIUM_LINK = 'https://medium.com/scrappy-squirrels';
+const contractAddress = "0x665D93FeddB6Ab546e77BeEdA307272B01665f79";
+const abi = contract.abi;
+
+const App = () => {
+
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [metamaskError, setMetamaskError] = useState(null);
+  const [mineStatus, setMineStatus] = useState(null);
+
+  const checkWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have Metamask installed!");
+      return;
+    } else {
+      console.log("Wallet exists! We're ready to go!")
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    const network = await ethereum.request({ method: 'eth_chainId' });
+
+    if (accounts.length !== 0 && network.toString() === '0x13881') {
+      const account = accounts[0];
+      console.log("Found an authorized account: ", account);
+      setMetamaskError(false);
+      setCurrentAccount(account);
+      //setupEventListener();
+    } else {
+      setMetamaskError(true);
+      console.log("No authorized account found");
+    }
+  }
+
+  const connectWallet = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Please install Metamask!");
+    }
+
+    try {
+      const network = await ethereum.request({ method: 'eth_chainId' });
+
+      if (network.toString() === '0x13881') {
+        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        console.log("Found an account! Address: ", accounts[0]);
+        setMetamaskError(null);
+        setCurrentAccount(accounts[0]);
+      }
+
+      else {
+        setMetamaskError(true);
+      }
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const mintNFT = async () => {
+    try {
+
+      setMineStatus('mining');
+
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const nftContract = new ethers.Contract(contractAddress, abi, signer);
+
+        console.log("Initialize payment");
+        let nftTxn = await nftContract.mintNFTs(1, { gasLimit: 160000, value: ethers.utils.parseEther("0.01") });
+
+        console.log("Mining... please wait");
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction: ${nftTxn.hash}`);
+        setMineStatus('success');
+
+      } else {
+        setMineStatus('error');
+        console.log("Ethereum object does not exist");
+      }
+
+    } catch (err) {
+      setMineStatus('error');
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    checkWalletIsConnected();
+
+    if (window.ethereum) {
+      window.ethereum.on('chainChanged', (_chainId) => window.location.reload());
+    }
+  }, [])
+
+  // Render Methods
+  const renderNotConnectedContainer = () => (
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
   );
-}
+
+  const renderMintUI = () => {
+    return (
+      <button onClick={mintNFT} className="cta-button connect-wallet-button" >
+        Mint a Polygon Squirrel NFT
+      </button >
+    );
+  }
+
+  return (
+    <Fragment>
+      {metamaskError && <div className='metamask-error'>Metamask から Polygon Testnet に接続してください!</div>}
+      <div className="App">
+        <div className="container">
+          <Header opensea={OPENSEA_LINK} />
+          <div className="header-container">
+            <div className='banner-img'>
+              <img src={squirrelImg} alt="Polygon Squirrels" />
+            </div>
+            {currentAccount && mineStatus !== 'mining' && renderMintUI()}
+            {!currentAccount && !mineStatus && renderNotConnectedContainer()}
+            <div className='mine-submission'>
+              {mineStatus === 'success' && <div className={mineStatus}>
+                <p>NFT minting successful!</p>
+                <p className='success-link'>
+                  <a href={`https://testnets.opensea.io/${currentAccount}/`} target='_blank' rel='noreferrer'>Click here</a>
+                  <span> to view your NFT on OpenSea.</span>
+                </p>
+              </div>}
+              {mineStatus === 'mining' && <div className={mineStatus}>
+                <div className='loader' />
+                <span>Transaction is mining</span>
+              </div>}
+              {mineStatus === 'error' && <div className={mineStatus}>
+                <p>Transaction failed. Make sure you have at least 0.01 MATIC in your Metamask wallet and try again.</p>
+              </div>}
+            </div>
+          </div>
+          {currentAccount && <div className='show-user-address'>
+          <p>
+            Your address being connected: &nbsp;
+            <br/>
+                <span>
+                    <a className='user-address' target='_blank' rel='noreferrer'>
+                        {currentAccount}
+                    </a>
+                </span>
+          </p>
+          </div>}
+          <Footer address={contractAddress} />
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
 export default App;
